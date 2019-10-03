fd73c62c5cc8331f96a126ef66d5ff3a7888032e
Piotr Delawski <delawski@users.noreply.github.com>
2019-10-03 15:37:23 +0200

    Add a test public API call with `apiFetch` package (#1)

--- src/hello-world/index.js
+++ src/hello-world/index.js
@@ -1,19 +1,46 @@
+/**
+ * WordPress dependencies
+ */
+import apiFetch from '@wordpress/api-fetch';
+import { useState, useEffect } from '@wordpress/element';
 import { Button, Panel, PanelBody, PanelRow } from '@wordpress/components';
 
-const HelloWorld = () => (
-	<Panel header="Test Panel">
-		<PanelBody
-			title="Toggle Panel"
-			icon=""
-			initialOpen={ true }
-		>
-			<PanelRow>
-				<Button isDefault>
-					Hello World!
-				</Button>
-			</PanelRow>
-		</PanelBody>
-	</Panel>
-);
+const rootURL = 'https://ma.tt/wp-json/';
+apiFetch.use( apiFetch.createRootURLMiddleware( rootURL ) );
+
+const HelloWorld = () => {
+	const [ posts, setPosts ] = useState( [] );
+
+	useEffect( () => {
+		apiFetch( { path: '/wp/v2/posts' } ).then( ( response ) => {
+			setPosts( response );
+		} );
+	}, [] );
+
+	return (
+		<Panel header="Test Panel">
+			<PanelBody
+				title="Toggle Panel"
+				icon=""
+				initialOpen={ true }
+			>
+				<PanelRow>
+					<Button isDefault>
+						Hello World!
+					</Button>
+				</PanelRow>
+				{ posts && (
+					<PanelRow>
+						<ul>
+							{ posts.map( ( { id, title } ) => (
+								<li key={ id } dangerouslySetInnerHTML={ { __html: title.rendered } } />
+							) ) }
+						</ul>
+					</PanelRow>
+				) }
+			</PanelBody>
+		</Panel>
+	);
+};
 
 export default HelloWorld;
