a7311e7d0c7f07355b32515b097ea2e5fe4eb4cd
Piotr Delawski <piotr.delawski@xwp.co>
2019-10-03 17:47:32 +0200

    Experiment with wpcom API calls using wpcom-proxy-request.

--- src/hello-world/index.js
+++ src/hello-world/index.js
@@ -4,16 +4,29 @@
 import apiFetch from '@wordpress/api-fetch';
 import { useState, useEffect } from '@wordpress/element';
 import { Button, Panel, PanelBody, PanelRow } from '@wordpress/components';
+import proxy from 'wpcom-proxy-request';
 
 const rootURL = 'https://ma.tt/wp-json/';
 apiFetch.use( apiFetch.createRootURLMiddleware( rootURL ) );
 
 const HelloWorld = () => {
 	const [ posts, setPosts ] = useState( [] );
+	const [ wpcomData, setWpcomData ] = useState( [] );
 
 	useEffect( () => {
 		apiFetch( { path: '/wp/v2/posts' } ).then( ( response ) => {
 			setPosts( response );
+		} );
+	}, [] );
+
+	useEffect( () => {
+		proxy( '/me', function( err, body, headers ) {
+			if ( err ) {
+				throw err;
+			}
+
+			console.log( body );
+			setWpcomData( body );
 		} );
 	}, [] );
 
@@ -36,6 +49,11 @@ const HelloWorld = () => {
 								<li key={ id } dangerouslySetInnerHTML={ { __html: title.rendered } } />
 							) ) }
 						</ul>
+					</PanelRow>
+				) }
+				{ wpcomData && (
+					<PanelRow>
+						{ wpcomData }
 					</PanelRow>
 				) }
 			</PanelBody>
