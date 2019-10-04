4e1902e8f0d7024715794daf00b2911125862100
Piotr Delawski <piotr.delawski@xwp.co>
2019-10-04 18:32:22 +0200

    Refine flows, add base stylesheet, add backups paging.

--- src/hello-world/index.js
+++ src/hello-world/index.js
@@ -3,20 +3,40 @@
  */
 import apiFetch from '@wordpress/api-fetch';
 import { useState, useEffect } from '@wordpress/element';
-import { Button, Panel, PanelBody, PanelRow } from '@wordpress/components';
+import { Button, Panel, PanelBody, PanelRow, Spinner } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+import { dateI18n } from '@wordpress/date';
 
 /**
  * External dependencies
  */
 import wpcom from 'wpcom';
 import wpcomOAuth from 'wpcom-oauth-cors';
+import { get } from 'lodash';
+
+const CLIENT_ID = '67055';
 
 const rootURL = 'https://ma.tt/wp-json/';
 apiFetch.use( apiFetch.createRootURLMiddleware( rootURL ) );
 
 const HelloWorld = () => {
+	const siteUrl = 'testsite108699957.blog';
 	const [ posts, setPosts ] = useState( [] );
-	const [ wpcomData, setWpcomData ] = useState( [] );
+	const [ api, setApi ] = useState( null );
+	const [ backupsPage, setBackupsPage ] = useState( 1 );
+	const [ totalBackupsPages, setTotalBackupsPages ] = useState( 999 );
+	const [ backups, setBackups ] = useState( [] );
+	const [ isLoading, setIsLoading ] = useState( false );
+
+	function loadMoreItems() {
+		if ( hasMorePages() ) {
+			setBackupsPage( backupsPage + 1 );
+		}
+	}
+
+	function hasMorePages() {
+		return backupsPage < totalBackupsPages;
+	}
 
 	useEffect( () => {
 		apiFetch( { path: '/wp/v2/posts' } ).then( ( response ) => {
@@ -25,36 +45,48 @@ const HelloWorld = () => {
 	}, [] );
 
 	useEffect( () => {
-		wpcomOAuth( '67055' );
+		if ( ! siteUrl || '' === siteUrl ) {
+			return;
+		}
+
+		wpcomOAuth( CLIENT_ID );
 		wpcomOAuth.get( ( auth ) => {
-			const api = wpcom( auth.access_token );
-			const site = api.site( auth.site_id );
-			site
-				.postsList( {
-					number: 8,
-				} )
-				.then( ( list ) => {
-					setWpcomData( list.posts );
-				} )
-				.catch( ( error ) => {
-					throw error;
-				} );
+			setApi( wpcom( auth.access_token ) );
 		} );
 	}, [] );
 
+	useEffect( () => {
+		if ( ! api ) {
+			return;
+		}
+
+		const query = {
+			apiNamespace: 'wpcom/v2',
+		};
+
+		setIsLoading( true );
+
+		api.req.get( `/sites/${ siteUrl }/activity?_envelope=1&aggregate=true&group%5B%5D=rewind&number=5&page=${ backupsPage }`, query, ( err, data ) => {
+			setIsLoading( false );
+
+			const totalPages = get( data, 'body.totalPages', totalBackupsPages );
+			setTotalBackupsPages( totalPages );
+
+			const items = get( data, 'body.current.orderedItems' );
+			if ( items ) {
+				setBackups( [ ... backups, ... items ] );
+			}
+		} );
+	}, [ api, backupsPage ] );
+
 	return (
-		<Panel header="Test Panel">
-			<PanelBody
-				title="Toggle Panel"
-				icon=""
-				initialOpen={ true }
-			>
-				<PanelRow>
-					<Button isDefault>
-						Hello World!
-					</Button>
-				</PanelRow>
-				{ posts && (
+		<Panel header={ __( 'API Requests Tests' ) }>
+			{ posts && (
+				<PanelBody
+					title={ __( 'Public API call' ) }
+					icon=""
+					initialOpen={ false }
+				>
 					<PanelRow>
 						<ul>
 							{ posts.map( ( { id, title } ) => (
@@ -62,18 +94,35 @@ const HelloWorld = () => {
 							) ) }
 						</ul>
 					</PanelRow>
-				) }
-				{ wpcomData && (
-					<PanelRow>
-						<ul>
-							{ wpcomData.map( ( { ID, title } ) => (
-								<li key={ ID }>
-									{ title }
-								</li>
-							) ) }
-						</ul>
-					</PanelRow>
-				) }
+				</PanelBody>
+			) }
+			<PanelBody
+				title={ __( 'Backups via authenticated call' ) }
+				icon=""
+				initialOpen={ true }
+			>
+				<PanelRow>
+					<ul>
+						{ backups.map( ( item ) => (
+							<li key={ item.activity_id }>
+								{ item.summary }, { dateI18n( 'F j, Y, h:m A', item.published ) }
+							</li>
+						) ) }
+						{ isLoading && (
+							<li>
+								<Spinner />
+							</li>
+						) }
+					</ul>
+				</PanelRow>
+				<PanelRow>
+					<Button
+						onClick={ loadMoreItems }
+						isPrimary
+						disabled={ ! hasMorePages() || isLoading }>
+						{ __( 'Load more items' ) }
+					</Button>
+				</PanelRow>
 			</PanelBody>
 		</Panel>
 	);
